63f8e45a030a4eccc9d251ad8fa62e7a5f6f8fdd
20520675 _TRANQUANGNHAT <95323123+Yamakaze-chan@users.noreply.github.com>
2023-01-03 21:32:51 +0700

    Add files via upload

---
+++ Sever/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace TCPSever
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.listClientIP = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(26, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sever: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(104, 29);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(430, 22);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "127.0.0.1:9000";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(104, 66);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInfo.Size = new System.Drawing.Size(430, 309);
+            this.txtInfo.TabIndex = 2;
+            this.txtInfo.Text = " ";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(104, 407);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(430, 22);
+            this.txtMessage.TabIndex = 3;
+            this.txtMessage.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(-2, 406);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(564, 406);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(645, 406);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // listClientIP
+            // 
+            this.listClientIP.FormattingEnabled = true;
+            this.listClientIP.ItemHeight = 16;
+            this.listClientIP.Location = new System.Drawing.Point(744, 66);
+            this.listClientIP.Name = "listClientIP";
+            this.listClientIP.Size = new System.Drawing.Size(396, 532);
+            this.listClientIP.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(741, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Client IP:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(541, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 309);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 700);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listClientIP);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " TCP/IP client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox listClientIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Sever/Form1.cs
@@ -0,0 +1,249 @@
+﻿using SuperSimpleTcp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TCPSever;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TCPSever
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer sever;
+        string temp_string;
+        string pp_send;
+        int receivedataimg;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            sever.Start();
+            txtInfo.Text += $"Starting...{Environment.NewLine}";
+            btnStart.Enabled = false;
+            btnSend.Enabled = true;
+        }
+
+        MemoryStream memoryStream = new MemoryStream(0);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtIP.Text.Trim();
+            btnStart.Enabled = true;
+            btnSend.Enabled = false;
+            sever = new SimpleTcpServer(txtIP.Text);
+            sever.Events.ClientConnected += Events_ClientConnected; ;
+            sever.Events.ClientDisconnected += Events_ClientDisconnected;
+            sever.Events.DataReceived += Events_DataReceived;
+            
+        }
+
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                //var str = System.Text.Encoding.Default.GetString(e.Data);
+                //string receive = Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count);
+                //bool isimg = false;
+                //if (receive.Contains("iushcxlchiasdchjaslfcdajhiodcadshjca"))
+                //{
+                //    isimg = true;
+                //    receive = Regex.Replace(receive, "[^0-9.]", "");
+                //}
+
+                //if(!isimg)
+                //{
+                //    string receive = Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count);
+                //    txtInfo.Text += $"{e.IpPort} : {receive}{Environment.NewLine}";
+                //    pp_send = e.IpPort;
+                //    temp_string = Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count);
+                //    btnSend_Click(sender, e);
+                //}
+                //else
+                //{
+                pp_send = e.IpPort;
+                memoryStream.Capacity = memoryStream.Capacity+e.Data.Count;
+                //while (e.Data.Count > 0)
+                //{
+                    memoryStream.Write(e.Data.Array, 0, e.Data.Count);
+
+
+
+                    //MessageBox.Show(e.Data.Count.ToString());
+
+                    //if (memoryStream.Length == int.Parse(receive))
+                    //{
+                    //    pictureBox1.Image = Image.FromStream(fs);
+                    //}
+                    //}
+                    try
+                    {
+                    Image.FromStream(memoryStream);
+                    //var myUniqueFileName = $@"{DateTime.Now.Ticks}.png";
+                    //Thread.Sleep(10);
+                    //MessageBox.Show("Done");
+                    //string path = "C:\\Users\\ACER\\Desktop\\picture\\outfile.png";
+                    string path = "outfile.png";
+                    FileStream fs = new FileStream(path, FileMode.Create);
+                    fs.Write(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+                    //memoryStream.SetLength(0);
+                    fs.Close();
+                    //using (FileStream fs = new FileStream(@"C:\Users\ACER\Desktop\picture\outfile.png", FileMode.Create))
+                    //{
+                    //    fs.Write(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+                    //    //fs.Flush();
+                    //}
+                    //MessageBox.Show((memoryStream.Capacity-receivedataimg).ToString());
+                    if(receivedataimg == memoryStream.Capacity)
+                    {
+                        //Thread.Sleep(1000);
+                        sendFile(path);
+                        memoryStream.Close();
+                        memoryStream.Dispose();
+                        memoryStream = new MemoryStream(0);
+                    }
+                    }
+                    catch (Exception ex)
+                    {
+                    //Thread.Sleep(10);
+                    
+                    string receive = Encoding.UTF8.GetString(memoryStream.ToArray());
+                    //MessageBox.Show(memoryStream.Capacity.ToString());
+                    if (receive.Contains("iushcxlchiasdchjaslfcdajhiodcadshjca"))
+                    {
+                        //MessageBox.Show(receive);
+                        receivedataimg = int.Parse(Regex.Replace(receive, "[^0-9.]", ""))+ memoryStream.Capacity;
+                        //Thread.Sleep(30);
+                        MessageBox.Show(this,"File has been sent");
+                        txtInfo.Text += $"{e.IpPort} : send picture with {receivedataimg} bytes {Environment.NewLine}";
+                        memoryStream.Close();
+                        memoryStream.Dispose();
+                        memoryStream = new MemoryStream(receivedataimg);
+                    }
+                    else
+                    {
+                        txtInfo.Text += $"{e.IpPort} : {receive}{Environment.NewLine}";
+                        //pp_send = e.IpPort;
+                        temp_string = Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count);
+                        btnSend_Click(sender, e);
+                        memoryStream.SetLength(0);
+                    }
+                }
+
+
+                //memoryStream.Capacity = memoryStream.Capacity + e.Data.Length;
+
+                //FileStream fs = new FileStream(@"C:\Users\ACER\Desktop\picture\outfile.png", FileMode.Create);
+                //fs.Write(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+                ////pictureBox1.Image = Image.FromStream(fs);
+                //fs.Close();
+                //    MessageBox.Show("Send success");
+                //}
+
+            });
+            //memoryStream.SetLength(0);
+            //MessageBox.Show(e.Data.Count.ToString());
+        }
+
+        private void Events_ClientConnected(object sender, ConnectionEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"{e.IpPort} connected.{Environment.NewLine}";
+                listClientIP.Items.Add(e.IpPort);
+            });
+        }
+
+        private void Events_ClientDisconnected(object sender, ConnectionEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                //memoryStream.SetLength(0);
+                txtInfo.Text += $"{e.IpPort} disconnected.{Environment.NewLine}";
+                listClientIP.Items.Remove(e.IpPort);
+            });
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(sever.IsListening)
+            {
+                if(listClientIP.Items.Count >0)
+                {
+                    foreach (var item in listClientIP.Items)
+                    {
+                        string ip = listClientIP.GetItemText(item);
+                        if(ip == pp_send)
+                        {
+                            continue;
+                        }
+                        string textmsg = pp_send + "//////" + temp_string;
+                        sever.Send(ip, textmsg);
+                        txtInfo.Text += $"{pp_send} {ip} {txtMessage.Text}{Environment.NewLine}";
+                        txtMessage.Text = "";
+                    }
+                }
+            }
+        }
+
+        private void sendFile(string filePath)
+        {
+            if(sever.IsListening)
+            {
+                if(listClientIP.Items.Count > 0)
+                {
+                    
+                    foreach (var item in listClientIP.Items)
+                    {
+                        string ip = listClientIP.GetItemText(item);
+                        if (ip == pp_send)
+                        {
+                            continue;
+                        }
+                        FileStream fs = new FileStream(filePath, FileMode.Open); ;
+                        long totalBytes = fs.Length;
+                        string senddata = "iushcxlchiasdchjaslfcdajhiodcadshjca" + totalBytes.ToString();
+                        byte[] byteArray = Encoding.UTF8.GetBytes(senddata);
+                        MemoryStream stream = new MemoryStream(byteArray);
+                        //MessageBox.Show(stream.Length.ToString());
+                        sever.Send(ip,stream.Length, stream);
+                        Thread.Sleep(50);
+                        sever.Send(ip, fs.Length, fs);
+                        Thread.Sleep(50);
+                        Thread.Sleep(200);
+                        fs.Close();
+                    }
+                    
+                    
+                }
+            }
+        }
+
+        public static bool IsValidImage(MemoryStream ms)
+        {
+            try
+            {
+                    Image.FromStream(ms);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
